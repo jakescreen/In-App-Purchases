c6ae476fdb2a35cf8de467a2d77d1fea66b91d1c
jakescreen <jscreen@udel.edu>
2021-09-21 18:13:57 -0400

    local storage to save progress in testing

--- js/crypto.js
+++ js/crypto.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
         count = count + 1;
         $("#counter").text(count);
     });
+
+    $("#saveButton").click(function(){
+        localStorage.setItem('Count', count);
+    });
+
+    $("#loadButton").click(function(){
+        count = +(localStorage.getItem('Count'));
+        $("#counter").text(count);
+    });
+
+
+
 });
