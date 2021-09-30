6691c5c2eadcea4abac9b3db5b745a6f0733dfc0
Jake Screen <jakescreen@wifi-roaming-128-4-67-121.wifi.udel.edu>
2021-09-30 10:32:19 -0400

    scaling issues may exist

--- js/crypto2.js
+++ js/crypto2.js
@@ -5,6 +5,25 @@ $('document').ready(() => {
         (btm==='0px')?closeMenu():openMenu();        
     });
 
+    let count = +($("#counter").text()); // initial count value
+    let delta = 1; // initial change in count per click
+    
+    $("#coinIcon").click(function(){
+        count = count + delta;
+        $("#counter").text(count);
+    });
+
+    $("#saveButton").click(function(){
+        localStorage.setItem('Count', count);
+        localStorage.setItem('Delta', delta);
+    });
+
+    $("#loadButton").click(function(){
+        count = +(localStorage.getItem('Count'));
+        delta = +(localStorage.getItem('Delta'));
+        $("#counter").text(count);
+    });
+
 });
 
 const closeMenu = () => {
