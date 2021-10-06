a7ecefa1e14f0115a21403bcfd30c7e5d5951446
Noah Katz <noahkatz@gmail.com>
2021-10-05 22:02:44 -0400

    updated validation

--- js/crypto3.js
+++ js/crypto3.js
@@ -1,6 +1,3 @@
-let username;
-let password;
-
 $('document').ready(() => {
 
     username = localStorage.getItem('username');
@@ -12,5 +9,26 @@ $('document').ready(() => {
     } else {
         $('.register').show();
     }
+    $('#submitLogin').on('click', ()=>{
+        if(document.getElementById('submitLogin').innerHTML=='Register') {
+            register($('#inputEmail').val(), $('#inputPassword1').val(), $('#inputPassword2').val());
+        }
+    });
 
 });
+
+const register = (email, p1, p2) => {
+    if(p1!=p2) {
+        alert('Passwords Do Not Match');
+        return;
+    }
+    if(!checkEmail(email)) {
+        alert('Invalid Email');
+        return;
+    }
+};
+
+const checkEmail = (email) => {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+};
