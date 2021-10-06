4bba96510b08e1b4469dce2d5da2e8d1574e0323
Noah Katz <noahkatz@gmail.com>
2021-10-06 00:58:18 -0400

    registration validation & redirect

--- js/crypto3.js
+++ js/crypto3.js
@@ -1,3 +1,5 @@
+console.log(window.location.search);
+
 $('document').ready(() => {
 
     username = localStorage.getItem('username');
@@ -14,21 +16,72 @@ $('document').ready(() => {
             register($('#inputEmail').val(), $('#inputPassword1').val(), $('#inputPassword2').val());
         }
     });
+    $('#inputPassword1').on('keyup', () => {
+        $('#passAlert').slideUp();
+        $('#passAlert2').slideUp();
+    });
+    $('#inputPassword2').on('keyup', () => {
+        $('#passAlert').slideUp();
+        $('#passAlert2').slideUp();
+    });
+    $('#inputEmail').on('keyup', () => {
+        $('#emailAlert').slideUp();
+    });
 
 });
 
 const register = (email, p1, p2) => {
-    if(p1!=p2) {
-        alert('Passwords Do Not Match');
-        return;
-    }
-    if(!checkEmail(email)) {
-        alert('Invalid Email');
-        return;
+    if(!validate(email, p1, p2)) {
+        window.location = `clicker.html?${email.substring(0, email.indexOf('@'))}`;
     }
 };
+
+const validate = (email, p1, p2) => {
+    let fail = 0;
+    let pa = "";
+    let ea = "";
+    if(!checkEmail(email)) {
+        ea = ('Invalid Email');
+        fail = 1;
+    }
+    if(!checkPassword(p1)) {
+        pa = ('Password must have at least eight characters, with at least one uppercase, one lowercase, and one number.');
+        fail = 1;
+    }
+    if(p1!=p2) {
+        pa = ('Passwords Do Not Match');
+        fail = 1;
+    }
+    passAlert(pa);
+    emailAlert(ea);
+    return fail;
+}
 
 const checkEmail = (email) => {
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
+};
+
+const checkPassword = (pass) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    return regex.test(pass);
+}
+
+const passAlert = (alert) => {
+    if(alert==="") return;
+    $('#passAlert').html(alert);
+    $('#passAlert2').html(alert);
+    $('#passAlert').slideDown();
+    $('#passAlert2').slideDown();
+}
+
+const emailAlert = (alert) => {
+    if(alert==="") return;
+    $('#emailAlert').html(alert);
+    $('#emailAlert').slideDown();
+}
+
+const submitLogin = (e) => {
+    const key = e.which;
+    if(key==13) $('#submitLogin').click();
 };
